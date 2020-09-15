abfbe088302db1fadbf9e4c549742b00138231ad
lohithmm <lohith.m@LOHITHs-MacBook-Air.local>
2020-09-15 15:41:18 +0530

    modified trav file

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-	    I was changed oh the feature branch and latest
+	    I was changed oh the feature branch and latest1
 	    </p>
       </div>
     );
